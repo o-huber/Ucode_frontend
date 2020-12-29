7305be12eb25205ef7708f638a1826f03df63ac1
Oleh Hubernator <50371410+o-huber@users.noreply.github.com>
2020-12-29 18:28:01 +0200

    update t03

---
+++ sprint02/t03/js/script.js
@@ -0,0 +1,11 @@
+'use script'
+function myFunc() {
+    alert("Freedom is power");
+}
+myFunc();
+
+function countMySquare(number) {
+    let square = number * number;
+    alert("The square of " + number + " = " + square);
+}
+countMySquare(2);
