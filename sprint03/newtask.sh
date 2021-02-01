fe4c00b9c9fc3351c28b7e03bc82e28037de2c62
Олег Губернатор <ohubernato@e1r6p8.unit.ua>
2021-02-01 13:32:41 +0200

    automation adding

---
+++ sprint03/newtask.sh
@@ -0,0 +1,27 @@
+#!/bin/zsh
+mkdir t$1
+mkdir t$1/js
+mkdir t$1/css
+touch t$1/index.html t$1/js/script.js t$1/css/style.css
+echo "<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>$2</title>
+  <meta charset=\"utf-8\">
+  <meta name=\"description\" content=\"t$1. $2\">
+  <meta name=\"author\" content=\"Oleh Hubernator\">
+  <meta name=\"keywords\" content=\"front-end, frontend, html,
+java script, javascript js, c, css, html5, css3\">
+  <meta name=\"viewport\" content=\"width=device-width, initial-scale-1\">
+   <link rel=\"stylesheet\" href=\"./css/style.css\">
+</head>
+
+<body>
+  <h1>$2</h1>
+  <script src=\"js/script.js\"></script>
+</body>
+
+</html>" >> t$1/index.html
+
+echo '"use strict"\n' >> t$1/js/script.js
